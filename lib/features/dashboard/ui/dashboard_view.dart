0c6763430ed23a23f7d5c59c74dff9b7df729685
Omar <omarplaystation8888@gmail.com>
2024-02-15 22:54:55 +0200

    - added mobile layout to DashboardView

--- lib/features/dashboard/ui/dashboard_view.dart
+++ lib/features/dashboard/ui/dashboard_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../core/utils/app_colors.dart';
 import '../../../core/widgets/adaptive_layout_widget.dart';
 import 'widgets/desk_top_layout.dart';
+import 'widgets/mobile_layout.dart';
 import 'widgets/tablet_layout.dart';
 
 class DashboardView extends StatelessWidget {
@@ -13,8 +14,9 @@ class DashboardView extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: AdaptiveLayoutWidget(
-        desktopLayout: (context) => const DeskTopLayout(),
+        mobileLayout: (context) => const MobileLayout(),
         tabletLayout: (context) => const TabletLayout(),
+        desktopLayout: (context) => const DeskTopLayout(),
       ),
     );
   }
